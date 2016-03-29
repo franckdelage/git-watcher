bdb0a7af4769ccaff6986507cecb59e67b69d5c6
Franck Delage <franck@web82.fr>
2016-03-29 10:40:16 +0200

    fix bad typo

--- watcher.js
+++ watcher.js
@@ -52,7 +52,7 @@ function getLog(callback) {
         "git",
         "--no-pager",
         "log",
-        "-color",
+        "--color",
         "--graph",
         "--pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'",
         "--abbrev-commit",
